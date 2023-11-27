8404a3d36856547b5e0d657935079e92f348f165
Karan Parekh <karanparekh501@gmail.com>
2023-11-27 18:43:39 +1100

    fixed broken appattack link

--- core/settings.py
+++ core/settings.py
@@ -152,10 +152,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'custom_static/'
+# STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'home/static',
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'custom_static')
+]
 
 
 #if not DEBUG:
