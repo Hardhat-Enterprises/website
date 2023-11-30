55b2b5275111a6ab53d8fd0e9d5d0a476194c8cd
221152992 <s221152992@deakin.edu.au>
2023-11-30 23:06:33 +1100

    Added login/sign-up page, modifications to views.py, urls.py, images are pulled from new files saved under 'custom_static' for more information check the html code

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ def index(request):
 
     # Page from the theme 
     return render(request, 'pages/index.html')
+
+def register_view(request):
+    return render(request, 'pages/register.html')
+
+def login_view(request):
+    return render(request, 'pages/login.html')
