3ce8a4b459cf1ea59565ef633083af741e1de857
aly <s222447649@deakin.edu.au>
2023-12-02 19:26:28 -0800

    malware viz main page

--- home/urls.py
+++ home/urls.py
@@ -4,11 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
     path('malware_viz/products_and_services', views.products_services, name='malware_products'),
-=======
     path('malware_viz', views.malwarehome, name='malware_viz_main'),
->>>>>>> 658d4be760c453d49c906210e35d451585d9e777
 ]
 
 
--- home/views.py
+++ home/views.py
@@ -7,13 +7,11 @@ def index(request):
     # Page from the theme 
     return render(request, 'pages/index.html')
 
-<<<<<<< HEAD
 def products_services(request):
     # Page from the theme 
     return render(request, 'pages/malware_visualization/products_and_services.html')
-=======
+
 def malwarehome(request):
 
     # Page from the theme 
     return render(request, 'pages/malware_visualization/main.html')
->>>>>>> 658d4be760c453d49c906210e35d451585d9e777
