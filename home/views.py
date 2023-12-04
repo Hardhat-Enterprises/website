3f43d7ae577545c2a58d0fb0ba1b7cd655330162
jalalferoj <j.feroj@gmail.com>
2023-12-04 21:55:13 +1100

    updated the link, hope it's ok

--- home/views.py
+++ home/views.py
@@ -5,10 +5,13 @@ from django.http import HttpResponse
 
 
 def index(request):
-<<<<<<< HEAD
-
     # Page from the theme
     return render(request, 'pages/index.html')
+
+
+def products_services(request):
+    # Page from the theme
+    return render(request, 'pages/malware_visualization/products_and_services.html')
 
 
 def malwarehome(request):
@@ -21,16 +24,3 @@ def malwareform(request):
 
     # Page from the theme
     return render(request, 'pages/malware_visualization/malware_viz_form.html')
-=======
-    # Page from the theme 
-    return render(request, 'pages/index.html')
-
-def products_services(request):
-    # Page from the theme 
-    return render(request, 'pages/malware_visualization/products_and_services.html')
-
-def malwarehome(request):
-
-    # Page from the theme 
-    return render(request, 'pages/malware_visualization/main.html')
->>>>>>> 0ac3b8bf6d1604da69002b656140ccba1268e38f
