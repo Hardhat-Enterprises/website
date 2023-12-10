056f39f7a15abcbb9ce062eec64a4cba6f076a06
Karan Parekh <karanparekh501@gmail.com>
2023-12-10 15:40:11 +1100

    added first and last name fields to the registration form

--- home/forms.py
+++ home/forms.py
@@ -3,39 +3,59 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, Pass
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
+from .models import Student
+
 User = get_user_model()
 
+def possible_years(first_year_in_scroll, last_year_in_scroll):
+    p_year = []
+    for i in range(first_year_in_scroll, last_year_in_scroll, -1):
+        p_year_tuple = str(i), i
+        p_year.append(p_year_tuple)
+    return p_year
+
 class RegistrationForm(UserCreationForm):
-  password1 = forms.CharField(
-      label=_("Your Password"),
-      widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
-  )
-  password2 = forms.CharField(
-      label=_("Confirm Password"),
-      widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
-  )
+    password1 = forms.CharField(
+        label=_("Your Password"),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+    )
+    password2 = forms.CharField(
+        label=_("Confirm Password"),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+    )
 
-  class Meta:
-    model = User
-    fields = ('email', )
 
-    labels = {
-      'email': _('Your Email'),
-    }
-    widgets = {
-      'email': forms.EmailInput(attrs={
-          'class': 'form-control',
-          'placeholder': 'example@deakin.edu.au'
-      })
-    }
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', )
+
+        labels = {
+            'first_name': _('First Name'),
+            'last_name': _('Last Name'),
+            'email': _('Deakin Email Address'),
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'First Name'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Last Name'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'example@deakin.edu.au'
+            })
+        }
 
 class UserLoginForm(AuthenticationForm):
-  username = UsernameField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}))
-  password = forms.CharField(
-      label=_("Password"),
-      strip=False,
-      widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}),
-  )
+    username = UsernameField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "example@deakin.edu.au"}))
+    password = forms.CharField(
+            label=_("Password"),
+            strip=False,
+            widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}),
+        )   
 
 class UserPasswordResetForm(PasswordResetForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={
