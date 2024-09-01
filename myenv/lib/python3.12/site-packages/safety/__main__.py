ff7e91fefceac6bef2810d2c3ecb3c7300e44e70
celine <s222197118@deakin.edu.au>
2024-09-02 04:47:28 +1000

    Updated project with Django 4.2.x LTS and all associated modules are kept up to date

--- myenv/lib/python3.12/site-packages/safety/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Allow safety to be executable through `python -m safety`."""
-from safety.cli import cli
-
-
-if __name__ == "__main__":  # pragma: no cover
-    cli(prog_name="safety")
--- myenv/lib/python3.12/site-packages/safety/scan/__init__.py
+++
@@ -1,7 +0,0 @@
-from rich.console import Console
-
-from safety_schemas.models import Vulnerability, RemediationModel
-from safety.scan.render import get_render_console
-console = Console()
-
-Vulnerability.__render__ = get_render_console(Vulnerability)
