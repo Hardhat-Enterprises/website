98a63f86e6a903632359a10d3cddbe1cfb804a62
celine <s222197118@deakin.edu.au>
2024-09-02 04:47:28 +1000

    Updated project with Django 4.2.x LTS and all associated modules are kept up to date

--- myenv/lib/python3.12/site-packages/safety/scan/finder/__init__.py
+++
@@ -1,7 +0,0 @@
-from .file_finder import FileFinder
-from .handlers import PythonFileHandler
-
-__all__ = [
-    "FileFinder",
-    "PythonFileHandler"
-]
--- myenv/lib/python3.12/site-packages/safety_schemas/models/util.py
+++
@@ -1,8 +0,0 @@
-
-from pydantic.version import VERSION as PYDANTIC_VERSION
-
-def dict_dump(obj):
-    if PYDANTIC_VERSION.startswith("1."):
-        return obj.dict()
-
-    return obj.model_dump()
