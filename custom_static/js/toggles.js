fcd47bf4f970d7ae4c3d1bf2ff4d9f7dfe401618
Alimumtaz123 <amumtaz932@gmail.com>
2024-12-04 01:11:11 +0500

    Dark mode button

--- custom_static/js/toggles.js
+++ custom_static/js/toggles.js
@@ -1,21 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('darkmode-toggle');
+    const toggleButton = document.getElementById('darkModeButton');
+    const icon = document.getElementById('darkModeIcon');
 
+    
     const darkMode = localStorage.getItem('darkMode') === 'true';
 
     if (darkMode) {
-        toggleButton.checked = darkMode;
         document.body.classList.add('dark-mode');
+        toggleButton.style.backgroundColor = '#333333'; 
+        icon.setAttribute('name', 'moon-sharp'); 
+        icon.style.color = '#ffcd11'; 
+    } else {
+        toggleButton.style.backgroundColor = '#f0f0f0'; 
+        icon.style.color = '#ffcd11'; 
     }
 
+    
     toggleButton.addEventListener('click', function () {
-        if(toggleButton.checked){
-            document.body.classList.add('dark-mode');
-            localStorage.setItem('darkMode', 'true');
-
-        }else{
+        if (document.body.classList.contains('dark-mode')) {
             document.body.classList.remove('dark-mode');
             localStorage.setItem('darkMode', 'false');
+            toggleButton.style.backgroundColor = '#f0f0f0'; 
+            icon.setAttribute('name', 'sunny-sharp'); 
+            icon.style.color = '#ffcd11'; 
+        } else {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('darkMode', 'true');
+            toggleButton.style.backgroundColor = '#333333'; 
+            icon.setAttribute('name', 'moon-sharp'); 
+            icon.style.color = '#ffcd11'; 
         }
     });
 });
