674084a8bea5e8f3b27d260b7ff6dea09c01bc61
gruhi patel <patelgruhi299@gmail.com>
2025-05-07 21:21:43 +1000

    Removed github field

--- home/forms.py
+++ home/forms.py
@@ -443,15 +443,11 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):  
     class Meta:
         model = Profile
-        fields = ['avatar', 'bio', 'github', 'phone', 'location']
+        fields = ['avatar', 'bio', 'phone', 'location']
         widgets = {
             'bio': forms.Textarea(attrs={
                 'class': 'form-control',
                 'placeholder': 'Tell us about yourself'
-            }),
-            'github': forms.URLInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'GitHub URL'
             }),
             'phone': forms.TextInput(attrs={
                 'class': 'form-control',
--- home/models.py
+++ home/models.py
@@ -341,7 +341,6 @@ class Profile(models.Model):
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     bio = models.TextField(max_length=500, blank=True, null=True)
     # linkedin = models.URLField(max_length=200, blank=True, null=True)
-    github = models.URLField(max_length=200, blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
 
