bde46feef3789fa4a74d5b0ef7d66895a06b6bb1
farhan <farhanahmedtaj@gmail.com>
2025-05-09 18:45:03 +1000

    features: 1. made cover letter upload same as resume upload. 2. added  carousel for life at hardhat on discover hardhat page. 3. Also added Hardhat by numbers section

--- home/forms.py
+++ home/forms.py
@@ -502,10 +502,7 @@ class JobApplicationForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}))
     resume = forms.FileField(widget=forms.ClearableFileInput(attrs={'class': 'form-control'}))
-    cover_letter = forms.FileField(
-        required=False,
-        label='Cover Letter (optional)',
-        widget=forms.ClearableFileInput(attrs={'accept': '.pdf,.doc,.docx'}))
+    cover_letter = forms.FileField(widget=forms.ClearableFileInput(attrs={'class': 'form-control'}))
 
 class PenTestingRequestForm(forms.ModelForm):
     terms_agreed = forms.BooleanField(required=True, label="I agree to the terms and conditions")
