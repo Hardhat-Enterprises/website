93b9775f8bfb55012b4c22d454a51e7202024d1e
Avishka Abeyrathna Mudumpitage <avishkaa342@gmail.com>
2025-07-29 16:47:34 +1000

    Tested if committing works

--- core/settings.py
+++ core/settings.py
@@ -276,6 +276,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 # For TLS, 465 for SSL
