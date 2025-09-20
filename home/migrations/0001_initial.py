1b26ea8a0554f1d7a1ebb6fd05a209bdbc3a85d5
MuhammadHaiderAli420 <93575502+MuhammadHaiderAli420@users.noreply.github.com>
2025-09-20 14:44:07 +1000

    Update 0001_initial.py

--- home/migrations/0001_initial.py
+++ home/migrations/0001_initial.py
@@ -571,7 +571,7 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-                        name='Folder',
+            name='Folder',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200)),
